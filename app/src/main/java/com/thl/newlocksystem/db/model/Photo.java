e7518fa53905c7a2457cd5344d76643cd79b8fa7
chenzongguo <1196437229@qq.com>
2019-04-22 22:37:03 +0800

    2

--- app/src/main/java/com/thl/newlocksystem/db/model/Photo.java
+++ app/src/main/java/com/thl/newlocksystem/db/model/Photo.java
@@ -8,4 +8,60 @@ public class Photo {
     String Photo_Desc = "Photo_Desc";//图片说明
     String Photo_Uploaded = "Photo_Uploaded";//上传标记
     String Photo_Uploaded_DateTime = "Photo_Uploaded_DateTime";//上传时间
+
+    public String getPhoto_Code() {
+        return Photo_Code;
+    }
+
+    public void setPhoto_Code(String photo_Code) {
+        Photo_Code = photo_Code;
+    }
+
+    public String getPhoto_Content() {
+        return Photo_Content;
+    }
+
+    public void setPhoto_Content(String photo_Content) {
+        Photo_Content = photo_Content;
+    }
+
+    public String getPhoto_InputTime() {
+        return Photo_InputTime;
+    }
+
+    public void setPhoto_InputTime(String photo_InputTime) {
+        Photo_InputTime = photo_InputTime;
+    }
+
+    public String getPhoto_Type() {
+        return Photo_Type;
+    }
+
+    public void setPhoto_Type(String photo_Type) {
+        Photo_Type = photo_Type;
+    }
+
+    public String getPhoto_Desc() {
+        return Photo_Desc;
+    }
+
+    public void setPhoto_Desc(String photo_Desc) {
+        Photo_Desc = photo_Desc;
+    }
+
+    public String getPhoto_Uploaded() {
+        return Photo_Uploaded;
+    }
+
+    public void setPhoto_Uploaded(String photo_Uploaded) {
+        Photo_Uploaded = photo_Uploaded;
+    }
+
+    public String getPhoto_Uploaded_DateTime() {
+        return Photo_Uploaded_DateTime;
+    }
+
+    public void setPhoto_Uploaded_DateTime(String photo_Uploaded_DateTime) {
+        Photo_Uploaded_DateTime = photo_Uploaded_DateTime;
+    }
 }
--- app/src/main/java/com/thl/newlocksystem/db/model/Unlock_Business.java
+++ app/src/main/java/com/thl/newlocksystem/db/model/Unlock_Business.java
@@ -27,4 +27,212 @@ public class Unlock_Business {
     String PPBS_Finish = "PPBS_Finish";//业务完成标记
     String PPBS_UpLoaded = "PPBS_UpLoaded";//上传标记
     String PPBS_UpLoaded_DateTime = "PPBS_UpLoaded_DateTime";//上传时间
+
+    public String getPPBS_Code() {
+        return PPBS_Code;
+    }
+
+    public void setPPBS_Code(String PPBS_Code) {
+        this.PPBS_Code = PPBS_Code;
+    }
+
+    public String getCus_Code() {
+        return Cus_Code;
+    }
+
+    public void setCus_Code(String cus_Code) {
+        Cus_Code = cus_Code;
+    }
+
+    public String getCompany_Code() {
+        return Company_Code;
+    }
+
+    public void setCompany_Code(String company_Code) {
+        Company_Code = company_Code;
+    }
+
+    public String getPPBS_Object() {
+        return PPBS_Object;
+    }
+
+    public void setPPBS_Object(String PPBS_Object) {
+        this.PPBS_Object = PPBS_Object;
+    }
+
+    public String getPPBS_Area() {
+        return PPBS_Area;
+    }
+
+    public void setPPBS_Area(String PPBS_Area) {
+        this.PPBS_Area = PPBS_Area;
+    }
+
+    public String getPPBS_AreaCode() {
+        return PPBS_AreaCode;
+    }
+
+    public void setPPBS_AreaCode(String PPBS_AreaCode) {
+        this.PPBS_AreaCode = PPBS_AreaCode;
+    }
+
+    public String getPPBS_Addr() {
+        return PPBS_Addr;
+    }
+
+    public void setPPBS_Addr(String PPBS_Addr) {
+        this.PPBS_Addr = PPBS_Addr;
+    }
+
+    public String getPPBS_Resaon() {
+        return PPBS_Resaon;
+    }
+
+    public void setPPBS_Resaon(String PPBS_Resaon) {
+        this.PPBS_Resaon = PPBS_Resaon;
+    }
+
+    public String getPPBS_ResaonCode() {
+        return PPBS_ResaonCode;
+    }
+
+    public void setPPBS_ResaonCode(String PPBS_ResaonCode) {
+        this.PPBS_ResaonCode = PPBS_ResaonCode;
+    }
+
+    public String getPPBS_LockType() {
+        return PPBS_LockType;
+    }
+
+    public void setPPBS_LockType(String PPBS_LockType) {
+        this.PPBS_LockType = PPBS_LockType;
+    }
+
+    public String getPPBS_LockTypeCode() {
+        return PPBS_LockTypeCode;
+    }
+
+    public void setPPBS_LockTypeCode(String PPBS_LockTypeCode) {
+        this.PPBS_LockTypeCode = PPBS_LockTypeCode;
+    }
+
+    public String getPPBS_Photo_Code() {
+        return PPBS_Photo_Code;
+    }
+
+    public void setPPBS_Photo_Code(String PPBS_Photo_Code) {
+        this.PPBS_Photo_Code = PPBS_Photo_Code;
+    }
+
+    public String getPPBS_Unlock_Person() {
+        return PPBS_Unlock_Person;
+    }
+
+    public void setPPBS_Unlock_Person(String PPBS_Unlock_Person) {
+        this.PPBS_Unlock_Person = PPBS_Unlock_Person;
+    }
+
+    public String getPPBS_110() {
+        return PPBS_110;
+    }
+
+    public void setPPBS_110(String PPBS_110) {
+        this.PPBS_110 = PPBS_110;
+    }
+
+    public String getPPBS_110Code() {
+        return PPBS_110Code;
+    }
+
+    public void setPPBS_110Code(String PPBS_110Code) {
+        this.PPBS_110Code = PPBS_110Code;
+    }
+
+    public String getPPBS_Bill_Code() {
+        return PPBS_Bill_Code;
+    }
+
+    public void setPPBS_Bill_Code(String PPBS_Bill_Code) {
+        this.PPBS_Bill_Code = PPBS_Bill_Code;
+    }
+
+    public String getPPBS_Unlock_DateTime() {
+        return PPBS_Unlock_DateTime;
+    }
+
+    public void setPPBS_Unlock_DateTime(String PPBS_Unlock_DateTime) {
+        this.PPBS_Unlock_DateTime = PPBS_Unlock_DateTime;
+    }
+
+    public String getPPBS_Unlock_Charge() {
+        return PPBS_Unlock_Charge;
+    }
+
+    public void setPPBS_Unlock_Charge(String PPBS_Unlock_Charge) {
+        this.PPBS_Unlock_Charge = PPBS_Unlock_Charge;
+    }
+
+    public String getPPBS_Material_Cost() {
+        return PPBS_Material_Cost;
+    }
+
+    public void setPPBS_Material_Cost(String PPBS_Material_Cost) {
+        this.PPBS_Material_Cost = PPBS_Material_Cost;
+    }
+
+    public String getPPBS_Total_Cost() {
+        return PPBS_Total_Cost;
+    }
+
+    public void setPPBS_Total_Cost(String PPBS_Total_Cost) {
+        this.PPBS_Total_Cost = PPBS_Total_Cost;
+    }
+
+    public String getPPBS_Reg_Person() {
+        return PPBS_Reg_Person;
+    }
+
+    public void setPPBS_Reg_Person(String PPBS_Reg_Person) {
+        this.PPBS_Reg_Person = PPBS_Reg_Person;
+    }
+
+    public String getPPBS_Reg_DateTime() {
+        return PPBS_Reg_DateTime;
+    }
+
+    public void setPPBS_Reg_DateTime(String PPBS_Reg_DateTime) {
+        this.PPBS_Reg_DateTime = PPBS_Reg_DateTime;
+    }
+
+    public String getPPBS_RegCompany_Code() {
+        return PPBS_RegCompany_Code;
+    }
+
+    public void setPPBS_RegCompany_Code(String PPBS_RegCompany_Code) {
+        this.PPBS_RegCompany_Code = PPBS_RegCompany_Code;
+    }
+
+    public String getPPBS_Finish() {
+        return PPBS_Finish;
+    }
+
+    public void setPPBS_Finish(String PPBS_Finish) {
+        this.PPBS_Finish = PPBS_Finish;
+    }
+
+    public String getPPBS_UpLoaded() {
+        return PPBS_UpLoaded;
+    }
+
+    public void setPPBS_UpLoaded(String PPBS_UpLoaded) {
+        this.PPBS_UpLoaded = PPBS_UpLoaded;
+    }
+
+    public String getPPBS_UpLoaded_DateTime() {
+        return PPBS_UpLoaded_DateTime;
+    }
+
+    public void setPPBS_UpLoaded_DateTime(String PPBS_UpLoaded_DateTime) {
+        this.PPBS_UpLoaded_DateTime = PPBS_UpLoaded_DateTime;
+    }
 }
